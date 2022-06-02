308fa62ad6c6a55a8d64b644443a0069c573dc92
KarenJPerez1280 <kjohanamelo@gmail.com>
2022-06-02 11:33:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ function showTemperature(response) {
     let min = Math.round(response.data.main.temp_min);
     let mintemp = document.querySelector(".lowOne");
     mintemp.innerHTML = `${min}°C`;
-    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/10d@2x.png`);
+    iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
 }
 
 function searchCity(city) {
